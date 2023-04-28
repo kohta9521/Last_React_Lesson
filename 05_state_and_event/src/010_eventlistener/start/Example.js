debcd3232822078a75e712977918196df3afc762
Kohta9521 <kohta0421kochi@gmail.com>
2023-04-28 13:17:55 +0900

    add first lesson about onClik AEvent

--- 05_state_and_event/src/010_eventlistener/start/Example.js
+++ 05_state_and_event/src/010_eventlistener/start/Example.js
@@ -1,6 +1,17 @@
 const Example = () => {
+  const ClickHandler = () => {
+    alert("クリックされました");
+  };
+
+  const ClickHandler2 = () => {
+    console.log("クリックされました");
+  };
+
   return (
-    <></>
+    <>
+      <button onClick={ClickHandler}>クリックしてね</button>
+      <button onClick={ClickHandler2}>クリックしてね</button>
+    </>
   );
 };
 
