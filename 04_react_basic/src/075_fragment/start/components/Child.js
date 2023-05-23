64330680fbf7244643c86d58d975119bb6ae9153
Kohta9521 <kohta0421kochi@gmail.com>
2023-05-23 17:19:30 +0900

    add react fragment

--- 04_react_basic/src/075_fragment/start/components/Child.js
+++ 04_react_basic/src/075_fragment/start/components/Child.js
@@ -1,10 +1,21 @@
 import "./Child.css";
+import React from "react";
 
 const Child = () => {
   return (
-    <div className="component">
-      <h3>Hello Component</h3>
-    </div>
+    // 画面に描画される際にはなかったものとみなされる
+    <React.Fragment>
+      <div className="component">
+        <h3>Hello Component</h3>
+      </div>
+      <h3>Hello Fragment</h3>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Harum quo
+        repudiandae iste ut, cupiditate, hic perspiciatis cum culpa corporis
+        suscipit dolor doloribus saepe, laboriosam architecto atque porro fuga
+        inventore vero.
+      </p>
+    </React.Fragment>
   );
 };
 
