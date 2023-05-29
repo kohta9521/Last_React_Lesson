afc1f61ae3c6be5d8e3e1ddae6460790a4baecd5
Kohta9521 <kohta0421kochi@gmail.com>
2023-05-30 06:36:01 +0900

    add 2023/05/30 first commit

--- 05_state_and_event/src/020_useState/start/Example.js
+++ 05_state_and_event/src/020_useState/start/Example.js
@@ -1,12 +1,5 @@
-import { useState } from "react";
-
 const Example = () => {
-  let valArry = useState(0);
-  return (
-    <>
-      <input type="text" onChange={(e) => e.target.value} /> = {valArry};
-    </>
-  );
+  return <></>;
 };
 
 export default Example;
