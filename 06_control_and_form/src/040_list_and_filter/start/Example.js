cbe317ec8a78ce030ae5d322f12b6e0ded04db78
Kohta9521 <kohta0421kochi@gmail.com>
2023-06-03 15:56:01 +0900

    add all file section about entryform section

--- 06_control_and_form/src/040_list_and_filter/start/Example.js
+++ 06_control_and_form/src/040_list_and_filter/start/Example.js
@@ -3,14 +3,22 @@ import { useState } from "react";
 const animals = ["Dog", "Cat", "Rat"];
 
 const Example = () => {
+  const [filterValue, setFilterValue] = useState("");
+  // console.log(animals.filter((animal) => animal === "dog"));
   return (
     <>
       <h3>配列のフィルター</h3>
+      <input
+        type="text"
+        value={filterValue}
+        onChange={(e) => setFilterValue(e.target.value)}
+      />
       <ul>
         {animals
+          .filter((animal) => animal.indexOf(filterValue) !== -1)
           .map((animal) => (
-          <li>{animal}</li>
-        ))}
+            <li key={animal}>{animal}</li>
+          ))}
       </ul>
     </>
   );
